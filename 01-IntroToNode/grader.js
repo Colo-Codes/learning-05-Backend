5b3b643d669a239f0b66369ef4776804aa98ec7d
Damian Demasi <damian.demasi.1@gmail.com>
2020-03-25 17:47:57 +1030

    Retouching exercise 2

--- 01-IntroToNode/grader.js
+++ 01-IntroToNode/grader.js
@@ -4,10 +4,10 @@ function average(array) {
         sum += element;
     });
     var average = Math.round(sum / array.length);
-    console.log(average);
+    return average;
 }
 // Using the function:
 var scores1 = [90, 98, 89, 100, 100, 86, 94];
-average(scores1);
+console.log(average(scores1));
 var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
-average(scores2);
+console.log(average(scores2));
