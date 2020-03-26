bd03a3004c666b5d6ebeaa44443057f140176601
Damian Demasi <damian.demasi.1@gmail.com>
2020-03-26 17:45:25 +1030

    npm: express: route parameters

--- 02-FirstExpressApp/app.js
+++ 02-FirstExpressApp/app.js
@@ -19,8 +19,23 @@ app.post('/dog', function(req, res) {
     res.send('Meow!!!');
 });
 
+// This is a route variable (using colon ":")
+app.get('/sub/:subPage', function(req, res) {
+    res.send('Welcome to your own sub-web page: ' + req.url);
+    console.log('Someone made a request to: ' + req.url);
+    console.log('The parameters are: ' + req.params);
+    console.log('The variable subPage = ' + req.params.subPage);
+});
+app.get('/sub/:subPage/:anotherSubPage', function(req, res) {
+    res.send('Welcome to your own sub-sub-web page! It\'s a rabbit\'s hole!');
+});
 
-// Tell Express to listen for requests (start server)
+// Listening in any other route. This needs to be after all other routes.
+app.get('*', function(req, res) {
+    res.send('Whoops! You are in the wrong place...');
+});
+
+// Tell Express to listen for requests (start server).
 app.listen(3000, function() {
     console.log('Serving web app on port 3000...');
     
