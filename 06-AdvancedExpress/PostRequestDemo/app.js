12a4a808a8316251d8308d352173283cae72395a
Damian Demasi <damian.demasi.1@gmail.com>
2020-03-31 17:20:46 +1030

    Post request v0.2

--- 06-AdvancedExpress/PostRequestDemo/app.js
+++ 06-AdvancedExpress/PostRequestDemo/app.js
@@ -1,6 +1,9 @@
 // Setting the "express" package:
 var express = require('express');
 var app = express();
+// Body Parser:
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 // View Engine:
 app.set('view engine', 'ejs');
 // Tell Express to listen for requests (start server):
@@ -8,19 +11,27 @@ app.listen(3000, function() {
     console.log('Serving web app on port 3000...');
 });
 
+// Making the array blobally visible to all routes:
+var friends = ['Jon', 'Lyanna', 'Harry', 'Hermione', 'Lara', 'Ron'];
+
 // Route requests:
 app.get('/', function(rew, res) {
     res.render('home');
 });
 
 app.get('/friends', function(req, res) {
-    var friends = ['Jon', 'Lyanna', 'Harry', 'Hermione', 'Lara', 'Ron']
     res.render('friends', {friendsWebpage: friends});
 });
 
 app.get('/addfriend', function(req, res) {
     res.send('You should be doing a POST request here...');
+    console.log('GET');
 });
 app.post('/addfriend', function(req, res) {
-    res.send('Post route!');
+    //console.log(req.body.newFriend); // Works with Body Parser
+    var newFriendPosted = req.body.newFriend;
+    friends.push(newFriendPosted);
+    console.log('POST');
+    // res.send('Post route! - <a href="/friends">Go back to /friends</a>');
+    res.redirect('/friends');
 });
