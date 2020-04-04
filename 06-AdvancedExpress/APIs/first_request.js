b44a15f9d5e03a61463907d49ce32cb64080270c
Damian Demasi <damian.demasi.1@gmail.com>
2020-04-04 11:24:28 +1030

    APIs - JSON and XML

---
+++ 06-AdvancedExpress/APIs/first_request.js
@@ -0,0 +1,22 @@
+// const request = require('request');
+// request('http://www.google.com', function (error, response, body) {
+//   console.error('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+// });
+
+var request = require('request');
+request('http://www.google.com', function(error, response, body){
+// request('http://www.google.notanurl', function(error, response, body){
+// request('http://www.google.com/000', function(error, response, body){
+    if (error) {
+        console.log('ERROR!!!!');
+    } else {
+        if (response.statusCode == 200) {
+            console.log('Worked 200 OK!');
+        } else {
+            console.log(response.statusCode);
+            
+        }
+    }
+});
