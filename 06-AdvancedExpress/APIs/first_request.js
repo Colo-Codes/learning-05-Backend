c3762a813c2711306cc6c01f007c8c339bdc374d
Damian Demasi <damian.demasi.1@gmail.com>
2020-04-05 09:44:28 +0930

    First API request

--- 06-AdvancedExpress/APIs/first_request.js
+++ 06-AdvancedExpress/APIs/first_request.js
@@ -6,13 +6,16 @@
 // });
 
 var request = require('request');
-request('http://www.google.com', function(error, response, body){
+// request('http://www.google.com', function(error, response, body){
 // request('http://www.google.notanurl', function(error, response, body){
 // request('http://www.google.com/000', function(error, response, body){
+// Weather API: http://api.weatherstack.com
+request ('http://api.weatherstack.com/current?access_key=0402f1fc062ef527e173b0267f15c67e&query=New%20York', function(error, response, body) {
     if (error) {
         console.log('ERROR!!!!');
     } else {
         if (response.statusCode == 200) {
+            console.log(response.body);
             console.log('Worked 200 OK!');
         } else {
             console.log(response.statusCode);
