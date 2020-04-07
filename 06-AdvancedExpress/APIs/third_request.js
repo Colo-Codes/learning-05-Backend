26fd386155b64831254e4f77ce69418a7f1f5f30
Damian Demasi <damian.demasi.1@gmail.com>
2020-04-07 17:05:07 +0930

    Third request

--- 06-AdvancedExpress/APIs/third_request.js
+++ 06-AdvancedExpress/APIs/third_request.js
@@ -1,14 +1,18 @@
-var request = require('request');
-var pry = require('pryjs');
-var locus = require('locus');
+// var request = require('request');
+// var pry = require('pryjs');
+// var locus = require('locus');
+const request = require('request');
+const pry = require('pryjs');
+const locus = require('locus');
 
-request ('http://api.weatherstack.com/current?access_key=0402f1fc062ef527e173b0267f15c67e&query=New%20York', function(error, response, body) {
+request ('http://api.weatherstack.com/current?access_key=0402f1fc062ef527e173b0267f15c67e&query=New%20York', (error, response, body) => {
     // eval(pry.it); // (!) It will not work if you have spaces in your working directory path.
     // eval(locus); // Works like a charm...
     if (!error && response.statusCode == 200) {
-        var bodyObject = JSON.parse(body);
+        const bodyObject = JSON.parse(body);
         // console.log('It is doing ' + bodyObject['current']['temperature'] + ' degrees in the city of ' + bodyObject['location']['name']);
-        console.log('It is doing ' + bodyObject.current.temperature + ' degrees in the city of ' + bodyObject.location.name);
+        // console.log('It is doing ' + bodyObject.current.temperature + ' degrees in the city of ' + bodyObject.location.name);
+        console.log(`It is doing ${bodyObject.current.temperature} degrees in the city of ${bodyObject.location.name}`);
     } else {
         console.log('Something went wrong! ' + response.statusCode);
     }
